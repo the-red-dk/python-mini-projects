412136655526abb8e260fd991123c7c943d17487
Daksha K Gowda <daksha01rdk@gmail.com>
2024-08-24 13:25:25 +0530

    MadLibs program with user input

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adjective = input("Enter an Adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter another adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter another adjective: ")
+
+
+print(f"Today I went to a {adjective} zoo.")
+print(f"In the exhibit, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
